ad8b8b6bfd6a3557a0246e73d2bfb1ad1cc10f17
alyx1017 <120736325+alyx1017@users.noreply.github.com>
2023-02-22 16:46:11 +0000

    Update whitelistedusers.lua

--- whitelistedusers.lua
+++ whitelistedusers.lua
@@ -4,3 +4,8 @@ local Whitelisted = {
     367404826;
     1394786071
 }
+
+if table.find(Whitelisted, game:GetService("Players").LocalPlayer.UserId) then
+elseif not table.find(Whitelisted, game:GetService("Players").LocalPlayer.UserId) then
+    game:GetService("Players").LocalPlayer:Kick()
+end
